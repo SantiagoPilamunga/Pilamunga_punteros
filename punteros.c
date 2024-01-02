dbc3ccacc3f5219cb80231967a204850c089ab13
Santiago <santiago.pilamunga@udla.edu.ec>
2024-01-02 15:20:24 -0500

    inicio2

--- punteros.c
+++ punteros.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main()
 {
+    // m es una variable de tipo puntero
     char *m,varChar;
+    varChar = 'A';
+    m= &varChar;
+    printf("%c %x",varChar,m);
     return 0;
 }
